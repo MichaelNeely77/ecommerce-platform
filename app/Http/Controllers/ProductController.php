9f0fa8bee15ae10c1741171c3780b8bf9ac3b64d
Michael Neely <michael@michaelneely.net>
2020-03-02 22:50:55 -0500

    Added the construct to ProductControllers

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,6 +9,10 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+    public function __construct() {
+        $this->middleware('auth:api')->except('index', 'show');
+    }
     /**
      * Display a listing of the resource.
      *

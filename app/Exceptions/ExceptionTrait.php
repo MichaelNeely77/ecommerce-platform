c2945871c92a10ddaccb4626d41f7e798230fe7a
Michael Neely <michael@michaelneely.net>
2020-03-08 22:58:53 -0400

    Added exceptiopns and made work

--- app/Exceptions/ExceptionTrait.php
+++ app/Exceptions/ExceptionTrait.php
@@ -18,6 +18,8 @@ trait ExceptionTrait
     if ($this->isHttp($e)) {
         return $this->HttpResponse($e);
         }
+
+        return parent::render($request, $e);
     }
 
     protected function isModel($e) {
